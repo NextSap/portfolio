3406c50deb127eff5458ccac6c5fd0cc80adde38
NextSap <nextsap@outlook.com>
2023-08-24 12:59:06 +0200

    update responsive design

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Skills from "@/components/Skills";
 
 const Home = () => {
     return (
-        <div className="h-[100vh] overflow-y-scroll">
+        <div className="h-[100vh]">
             <Hello/>
             <About/>
             <Projects/>
